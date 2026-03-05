b3e61f2b06f269341540eb2b266a2abca2156b1f
NeyiSoek <erikleonel-lp@outlook.com>
2026-03-05 10:27:51 -0800

    Add security rules and restrictions for Guardias in hr.leave and employee models

--- modules/security_view_time_off/__manifest__.py
+++ modules/security_view_time_off/__manifest__.py
@@ -8,10 +8,13 @@
     'website': "https://www.fruvemex.com",
     'category': 'Human Resources',
     'version': '0.1',
-    'depends': ['base', 'hr','hr_attendance','hr_holidays','zkteco_realtime_connector'],
+    'depends': ['base', 'hr','hr_attendance','hr_holidays','zkteco_realtime_connector', 'employee_modifications'],
 
     'data': [
         'security/security.xml',
+        'security/hr_leave_guardia_rules.xml',
+        'security/employee_field_restrictions.xml',
+        'security/hr_leave_guardia_field_restrictions.xml',
         'security/ir.model.access.csv',
         'views/time_off_security_view.xml',
         'views/time_off_security_menu.xml',
